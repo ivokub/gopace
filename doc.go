abee06ea8fd9bf1ec8f871a6eaa49b3c4d77b7fa
Ivo Kubjas <ivo@teokon.eu>
2021-02-07 21:07:48 +0100

    rename package name in documentation

--- doc.go
+++ doc.go
@@ -1,2 +1,2 @@
-// Package go-pace-esteid allows to establish a secure channel between card and host using PACE protocol.
+// Package gopace allows to establish a secure channel between card and host using PACE protocol.
 package gopace
